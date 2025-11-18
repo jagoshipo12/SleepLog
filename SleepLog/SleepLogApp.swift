3fbb0561a7c86108dae788a654d2d22fbeb3517d
jagoshipo12 <rin4883@daum.net>
2025-11-18 15:47:33 +0900

    Initial Commit

---
+++ SleepLog/SleepLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepLogApp.swift
+//  SleepLog
+//
+//  Created by DSU_student on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
